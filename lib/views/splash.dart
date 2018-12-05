6bcfcd756ceb83e67a3e47c83b4a505947463b56
lijunen <lijunen.happy@163.com>
2018-12-05 22:25:43 +0800

    启动页

--- lib/views/splash.dart
+++ lib/views/splash.dart
@@ -1,12 +1,61 @@
 import 'package:flutter/material.dart';
-class SplashPage extends StatefulWidget{
-  SplashState createState()=>SplashState();
+import 'package:common_utils/common_utils.dart';
+class SplashPage extends StatefulWidget {
+  SplashState createState() => SplashState();
 }
 
-class SplashState extends State{
-  Widget build(BuildContext context){
-    return Container(
-      child:new Image.asset('assets/start.jpg',width: double.infinity,height: double.infinity,),
+class SplashState extends State {
+  int timer = 3;
+  @override
+  initState(){
+    super.initState();
+    _countDown();
+  }
+  void _goHomePage(){
+    Navigator.of(context).pushReplacementNamed('/homepage');
+  }
+  void _countDown(){
+    TimerUtil countDown = new TimerUtil();
+    countDown.setInterval(1000);
+    countDown.setTotalTime(3*1000);
+    countDown.setOnTimerTickCallback((int value){
+      timer--;
+      if(timer == 0){
+        _goHomePage();
+      }
+      setState(() {
+        timer = timer;
+      });
+    });
+    countDown.startCountDown();
+//    new Future.delayed(new Duration(seconds: 3),(){
+//      _goHomePage();
+//    });
+
+  }
+
+  Widget build(BuildContext context) {
+    return new Stack(
+      children: <Widget>[
+        Container(
+          child: new Image.asset(
+            'assets/start.jpg',
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ),
+        new ClipOval(
+          child: new Align(
+            alignment: Alignment.topRight,
+            child: new Container(
+              width: 30.0,
+              height: 30.0,
+              decoration: new BoxDecoration(color: Colors.lightBlue),
+              child: new Text(timer.toString(),style: new TextStyle(fontSize: 20,color: Colors.red),),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
